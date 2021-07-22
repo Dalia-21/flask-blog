c93cace34feb0f5a91e07f4171ee145c9bb0c0cd
Dalia-21 <dalia.littler@protonmail.com>
2021-07-22 11:26:52 +0200

    support for error message display added

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ def create_app():
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
     bootstrap.init_app(app)
 
     return app
