957733ee3dbfd0b9b82e7a6f4b81610ea913a4cf
Dalia-21 <dalia.littler@protonmail.com>
2021-07-22 11:03:39 +0200

    created basic base template using bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
+
+bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+    bootstrap.init_app(app)
 
     return app
