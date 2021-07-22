7b57ba9b8f40d16d9770fecdcdcb59ea870332c8
Dalia-21 <dalia.littler@protonmail.com>
2021-07-22 18:50:54 +0200

    user db added

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-
+db = SQLAlchemy()
+migrate = Migrate()
 bootstrap = Bootstrap()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
@@ -21,3 +27,5 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
 
     return app
+
+from app import models
