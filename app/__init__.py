f3e820789ac8d0467a7eb99102f11d34751aefc9
Dalia-21 <dalia.littler@protonmail.com>
2021-07-23 12:56:47 +0200

    login and registration forms now submit to database

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from flask_bootstrap import Bootstrap
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
 bootstrap = Bootstrap()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please log in to access this page.'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -14,6 +18,7 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
