333d3c0e3083ba693a387b463dd5ac09c6a2f2b3
Dalia-21 <dalia.littler@protonmail.com>
2021-07-27 11:58:51 +0200

    refactored so that links from home go to posts

--- app/main/routes.py
+++ app/main/routes.py
@@ -8,3 +8,10 @@ from app.models import Post
 def index():
     posts = Post.query.all()
     return render_template('index.html', title='Home', posts=posts)
+
+
+@bp.route('/post/<post_id>')
+def view_post(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    return render_template('post.html', title=post.title,
+                           post=post)
