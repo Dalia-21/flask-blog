280b44e26af15febd4959af8b189e28157968af2
Dalia-21 <dalia.littler@protonmail.com>
2021-08-03 15:16:29 +0200

    users can now edit their profiles

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,11 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
 from datetime import datetime
 
 from app.models import Post, User
 from app import db
 from app.main import bp
+from app.main.forms import EditProfileForm, EditCredentialsForm
 
 
 @bp.before_app_request
@@ -32,9 +33,30 @@ def view_post(post_id):
                            post=post)
 
 
-@bp.route('/user/<user_id>')
-@login_required
+@bp.route('/user/<user_id>', methods=['GET', 'POST'])
+@login_required  # also need to ensure user matches logged in user
 def profile(user_id):
     user = User.query.filter_by(id=user_id).first_or_404()
+    profile_form = EditProfileForm()
+    credentials_form = EditCredentialsForm()
+    if profile_form.validate_on_submit():
+        if profile_form.username.data:
+            user.username = profile_form.username.data
+        if profile_form.about_me.data:
+            user.about_me = profile_form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('Profile data successfully updated.')
+    elif credentials_form.validate_on_submit():
+        if not user.check_password(credentials_form.password.data):
+            flash('Incorrect password.')
+            return redirect(url_for('main.profile', user_id=current_user.id))
+        user.email = credentials_form.email.data
+        if credentials_form.new_password:
+            user.set_password(credentials_form.new_password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Credentials successfully updated.')
     return render_template('profile.html', title=user.username,
-                           user=user)
+                           user=user, profile_form=profile_form,
+                           credentials_form=credentials_form)
