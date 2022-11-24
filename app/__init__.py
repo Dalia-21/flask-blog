0b51781a9c6a2e071ce9e8edf679c42c80f44f44
Dalia-21 <dalia.sinclair@protonmail.com>
2022-11-24 20:19:57 +1100

    remove unneeded import

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from config import Config
 from config import env_override
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 
 
 db = SQLAlchemy()
