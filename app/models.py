c0101a873569c034cbdd942e4c27d11199151a28
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-07 16:27:41 +1100

    add flask-security classes to app.models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from app import db
+from flask_security import UserMixin, RoleMixin
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +11,33 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Title: {}>\nPost {}'.format(self.title, self.body)
+
+
+class RolesUsers(db.Model):
+    __tablename__ = 'roles_users'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    role_id = db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))  # will this be hashed?
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(db.String(100))
+    current_login_ip = db.Column(db.String(100))
+    login_count = db.Column(db.Integer)
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary='roles_users',
+                            backref='users', lazy='dynamic')
