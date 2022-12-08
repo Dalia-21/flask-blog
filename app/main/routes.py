4beda0eaea265ce12de72bb5bf45d426ed96b4d3
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-08 12:09:34 +1100

    add login function for admin page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import render_template, url_for, abort, flash, request, redirect
+from flask_login import login_user
+from flask_admin.helpers import is_safe_url
 from app.main import bp
 from app.models import Post
 
@@ -18,3 +20,19 @@ def view_post(post_id):
     post = Post.query.filter_by(id=post_id).first_or_404()
     return render_template('post.html', title=post.title,
                            post=post)
+
+
+@bp.route('/admin/login', methods=['GET', 'POST'])
+def admin_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        login_user(form.user)
+        flash('Logged in successfully.')
+
+        next = request.args.get('next')
+
+        if not is_safe_url(next):
+            return abort(400)
+
+        return redirect(next or url_for('admin'))
+    return render_template('login.html', form=form)
